d3c4e6951543c451b9b52ff95e0d7a78f134673b
Kruceo <rafa2015krueger@gmail.com>
2022-10-25 16:18:22 -0300

    att: limits implement

--- index.js
+++ index.js
@@ -5,18 +5,53 @@ export function initWoodPicker() {
     let isRolling = 'stopped'
     let history = []
     let baseBottom = target.style.marginBottom ?? getComputedStyle(target).marginBottom ?? 0
+    let debug = debugCreate('blue')
+    let debu = debugCreate()
     scrollTarget.addEventListener('scroll', (e) => {
-        let coef = (baseBottom - scrollTarget.scrollY * 1)
-        let limit = (window.visualViewport.height - target.offsetHeight/1.5) < target.offsetHeight + coef
-        console.log(coef,target.offsetHeight* 0.75)
-        if(limit)
-        {
-            target.style.marginBottom = coef + 'px'
-        }
        
+      
+
+
+        let marginToAdd = target.offsetTop + (baseBottom - scrollTarget.scrollY * 1)
+        let limit = (target.offsetTop + target.offsetHeight)+ marginToAdd
+        let coef = target.offsetHeight - marginToAdd;
+        let screenHeight = window.visualViewport.height
+        let scrollB = scrollTarget.scrollY + scrollTarget.visualViewport.height;
+        let isInScreen = false
+        HTMLDivElement.prototype
+        isInScreen = false
+        if(limit > scrollTarget.scrollY && target.offsetTop < scrollB)
+        {
+            target.style.marginBottom = marginToAdd + 'px'
+            isInScreen = true
+        }
+        console.log(' \nmar:'+marginToAdd.toFixed(0).padEnd(6,' ')
+        +' |\n top :',target.offsetTop 
+        +' |\n scrT :',scrollTarget.scrollY
+        +' |\n scrB:',scrollB
+        +' |\n coe :',coef.toFixed(0)
+        +' |\n lim :',limit
+        +' |\n ini :',target.offsetTop 
+        +' |\n show :',isInScreen)
+       
+       
+        debu.style.top = limit+ 'px';
+        debug.style.top = target.offsetTop + marginToAdd+ 'px';
         target.style.position = 'relative'
         target.style.display = 'block'
         
         
     })
+    document.body.appendChild(test)
+    
+}
+
+function debugCreate(color,name)
+{
+    let test = document.createElement('a')
+    test.style.position = 'absolute'
+    test.style.color = color??'red'
+    test.innerHTML = name??'debug'+("=".repeat(100))
+    document.body.appendChild(test)
+    return test
 }
