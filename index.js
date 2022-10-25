d2a4255816d14d7919f2339554a321268ce29ac5
Kruceo <rafa2015krueger@gmail.com>
2022-10-25 13:13:56 -0300

    att: limit implement

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-function initWoodPicker(){
-
-let isRolling = 'stopped'
-let history = []
-let baseBottom = document.querySelector('parallax').style.marginBottom??getComputedStyle(document.querySelector('parallax')).marginBottom??0
-window.addEventListener('scroll', (e) => {
-    document.querySelector('parallax').style.marginBottom = baseBottom -  window.scrollY *  .75+ 'px'
-    document.querySelector('parallax').style.position = 'relative'
-    document.querySelector('parallax').style.display = 'block'
-    if (document.querySelector('parallax').style.marginBottom < 100) {
-        document.querySelector('parallax').style.marginBottom = '100px'
-    }
-    history.push(window.scrollY.toFixed(0))
-    if (history.length > 10) {
-        history = []
-    }
-    if (history[history.length - 1] > history[history.length - 2]) {
+initWoodPicker()
+function initWoodPicker() {
+    let target = document.querySelector('parallax');
+    let scrollTarget = window;
+    let isRolling = 'stopped'
+    let history = []
+    let baseBottom = target.style.marginBottom ?? getComputedStyle(target).marginBottom ?? 0
+    scrollTarget.addEventListener('scroll', (e) => {
+        let coef = (baseBottom - scrollTarget.scrollY * .75)
+        console.log(target.offsetHeight+'  ==  '+(target.offsetHeight + coef).toFixed(0))
+        if(target.offsetHeight/2 < target.offsetHeight + coef)
+        {
+            target.style.marginBottom = coef + 'px'
+        }
+       
+        target.style.position = 'relative'
+        target.style.display = 'block'
         
-        isRolling = 'down'
-    }
-    if (history[history.length - 1] < history[history.length - 2]) {
         
-        isRolling = 'up'
-    }
-    if (history[history.length - 1] == history[history.length - 2]) {
-        isRolling = 'stopped'
-    }
-    console.log(isRolling)
-})
-
-    `
-1000 = 100
-333 = x
-
-333 * 100
-1000
-
-`
+    })
 }
